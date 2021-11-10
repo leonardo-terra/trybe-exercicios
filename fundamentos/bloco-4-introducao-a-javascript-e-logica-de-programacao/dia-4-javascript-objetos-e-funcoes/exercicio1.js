7d088991a3f1ae810431842812a2a4c84aa5808a
leonardo-terra <leo.terra.melo@gmail.com>
2021-11-10 19:13:30 -0300

    Exercicios objetos completos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio1.js
@@ -1,4 +1,4 @@
-let info = {
+/* let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
@@ -21,5 +21,30 @@ let info = {
       console.log("Ambos são");
     }else{
     console.log(info[key]+" e " + info2[key]);
-    }
-  }
+    }q
+  } */
+
+  let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+  
+//console.log("O livro favorito de "+ leitor.nome+ " "+ leitor.sobrenome+ " é: "+ leitor['livrosFavoritos'][0]['titulo'] + '"');
+
+leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+  },
+);
+//console.table(leitor);
+
+console.log("Julia tem" + leitor.livrosFavoritos.length + " livros favoritos");
