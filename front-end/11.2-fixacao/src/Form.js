c691876279fd7fd8fceab206a3bf33c91efea88a
leonardo-terra <leo.terra.melo@gmail.com>
2022-02-01 15:42:08 -0300

    adição css

--- front-end/11.2-fixacao/src/Form.js
+++ front-end/11.2-fixacao/src/Form.js
@@ -1,36 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Form.css';
 
-class Form extends React.Component {
+class Form extends Component {
 	constructor() {
 		super();
-		
-        this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
 
-        this.state = {
-			aboutYou: '',
-            name: '',
+		this.handleChange = this.handleChange.bind(this);
+
+		this.state = {
+			estadoFavorito: '',
 		};
 	}
 
-    handleTextAreaChange (event) {
-        this.setState({aboutYou: event.target.value})
-    }
+	handleChange(event) {
+		this.setState({
+			estadoFavorito: event.target.value,
+		});
+	}
 
 	render() {
 		return (
 			<div>
-				<label>
-					About you:
-					<input name='aboutYou' 
-                        value = {this.state.aboutYou} 
-                        type='text' 
-                        onChange={this.handleTextAreaChange}>
-                    </input>
-				</label>
-                <label>
-					Nome:
-					<input name='name' type='text'></input>
-				</label>
+				<h1>
+				Estados e Componentes controlados
+				</h1>
+				<form className='form'>
+					<label>
+						Diga qual o seu Estado favorito:
+						<textarea
+							name='estadoFavorito'
+							value={this.state.estadoFavorito}
+							onChange={this.handleChange}
+						/>
+					</label>
+					<input type='number' name='idade' />
+					<input type='checkbox' name='vaiComparecer' />
+				</form>
 			</div>
 		);
 	}
