f3ff7d61f9bfcbcb43486ccd48c026430ed33977
leonardo-terra <leo.terra.melo@gmail.com>
2022-02-01 13:49:44 -0300

    exercicio fixacao

---
+++ front-end/11.1 - exercicio-fixacao/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			numeroDeCliques: 0,
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	   handleClick = () => {
+		this.setState((estadoAnterior, _props) => ({
+			numeroDeCliques: estadoAnterior.numeroDeCliques + 1,
+		}));
+	};
+
+	render() {
+		return (
+			<button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
+		);
+	}
+}
+
+export default App;
