84dc6314399a0ee8464d3718042efd3616dfe86b
leonardo-terra <leo.terra.melo@gmail.com>
2022-03-09 11:30:19 -0300

    Redux implementado e conectado

--- Aulas/15.3 - Gerenciando estados/app-do-zero/src/App.js
+++ Aulas/15.3 - Gerenciando estados/app-do-zero/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-    </>
-  );
+import AppRoutes from './routes';
+
+class App extends React.Component{
+  render(){
+    return(
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
+x
