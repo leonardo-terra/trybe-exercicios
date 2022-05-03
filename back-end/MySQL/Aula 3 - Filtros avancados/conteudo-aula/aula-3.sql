f632fc0abaaa5c14e7b80dd41a3dacb3683fa4dd
leonardo-terra <leo.terra.melo@gmail.com>
2022-05-03 15:05:28 -0300

    mysql querys exercises

--- back-end/MySQL/Aula 3 - Filtros avancados/conteudo-aula/aula-3.sql
+++ back-end/MySQL/Aula 3 - Filtros avancados/conteudo-aula/aula-3.sql
@@ -6,3 +6,19 @@ SELECT * FROM sakila.address
 WHERE district IN ('QLD', 'Nagasaki', 'California', 'Attika', 'Mandalay', 'Nantou', 'Texas')
 ;
 
+SELECT title, length FROM sakila.film
+WHERE length BETWEEN 50 AND 120
+;
+
+SELECT * FROM sakila.language
+WHERE name BETWEEN 'Italian' AND 'Mandarin'
+ORDER BY name
+;
+
+SELECT rental_id, rental_date FROM sakila.rental
+WHERE rental_date
+BETWEEN '2005-05-27' AND '2005-07-17'
+;
+
+SELECT * FROM sakila.payment
+;
