99d6a505ba538827eefe7be33b7e5be0df0250f9
leonardo-terra <leo.terra.melo@gmail.com>
2022-05-08 16:33:12 -0300

    exercises from class 21.2

---
+++ back-end/MySQL/aula 5 - JOINS/exercicios.sql
@@ -0,0 +1,8 @@
+SELECT * FROM sakila.actor;
+
+SELECT * FROM sakila.film_actor;
+
+SELECT a.actor_id, a.first_name, f.film_id
+FROM sakila.actor a
+INNER JOIN sakila.film_actor f
+ON a.actor_id = f.actor_id
