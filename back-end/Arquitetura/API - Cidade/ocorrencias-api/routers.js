75b87acc1a63a5098a0f16db928cbb588d33cf8c
leonardo-terra <leo.terra.melo@gmail.com>
2022-05-25 18:06:19 -0300

    First commit - API ocorrencias

--- back-end/Arquitetura/API - Cidade/ocorrencias-api/routers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-//Health Checker controller
-router.use('/healthchecker', require('./controllers/healthchecker'));
-
-module.exports = router;
