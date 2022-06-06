ad6b0f419320c1e399d02a495618bb09a8f5e34e
leonardo-terra <leo.terra.melo@gmail.com>
2022-06-06 15:49:16 -0300

    Gera tabela Users

---
+++ back-end/ORM/aula-24.1/models/user.js
@@ -0,0 +1,30 @@
+/* 'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+
+    static associate(models) {
+    }
+  }
+  User.init({
+    fullName: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+}; */
+
+//Trabalharemos com funções por enquanto
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  });
+
+  return User;
+};
+
+module.exports = User;
